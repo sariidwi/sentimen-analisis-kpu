1271ce7418826b9f121a7cec6e74d7bbe3102b15
dwiramadhaniasarii <dwiramadhaniasari.21057@mhs.unesa.ac.id>
2025-06-03 10:43:55 +0700

    Initial commit

--- config.py
+++ config.py
@@ -4,7 +4,8 @@
 DB_USER = 'root'
 DB_PASSWORD = 'anjay123'
 DB_HOST = 'localhost'
-DB_NAME = 'Sentimen_Sirekap'
+DB_NAME = 'sirekap_sentimen'
 
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://root:anjay123@localhost/Sentimen_Sirekap'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = os.environ.get('SECRET_KEY', 'rahasia-aman-123')
